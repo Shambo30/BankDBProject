731fbad16948e6818f2334b8368fe837ec460fc9
Marcus Dickson <124563538+mdickson05@users.noreply.github.com>
2024-10-05 11:24:31 +0800

    FEAT: Added login pages for user and admin

---
+++ BankWebService/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebService.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
