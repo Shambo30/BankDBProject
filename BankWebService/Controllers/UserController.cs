a9cb2f2213104f98358a79d6c1b47f0f9763f58d
Marcus Dickson <124563538+mdickson05@users.noreply.github.com>
2024-10-05 11:32:18 +0800

    FEAT: Added navigation to user and admin dashboards

---
+++ BankWebService/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebService.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
